1909c44c51c3637d3a161da3130f9906602e5670
Frinter <codewad@gmail.com>
2015-11-11 17:52:16 -0800

    Add rendering of tiles from a tilemap

--- src/adsrenderer.cc
+++ src/adsrenderer.cc
@@ -16,6 +16,28 @@ using std::endl;
 using std::string;
 using std::vector;
 
+const float imageVertices[] = {
+    1.0f, 1.0f, 0.0f, 1.0f,
+    0.0f, 1.0f, 0.0f, 1.0f,
+    0.0f, 0.0f, 0.0f, 1.0f,
+    1.0f, 1.0f, 0.0f, 1.0f,
+    0.0f, 0.0f, 0.0f, 1.0f,
+    1.0f, 0.0f, 0.0f, 1.0f
+};
+
+const float imageNormals[] = {
+    0.0f, 0.0f, 1.0f,
+    0.0f, 0.0f, 1.0f,
+    0.0f, 0.0f, 1.0f,
+    0.0f, 0.0f, 1.0f,
+    0.0f, 0.0f, 1.0f,
+    0.0f, 0.0f, 1.0f
+};
+
+const IndexValue imageIndices[] = {
+    0, 1, 2, 3, 4, 5
+};
+
 template <class T>
 class VertexArrayBuffer
 {
@@ -37,10 +59,18 @@ public:
         Bind();
         glVertexAttribPointer(index, size, type, normalized, stride, pointer);
     }
-    
+
     void Bind()
     {
         glBindBuffer(_targetType, _bufferHandle);
+    }
+
+    IndexValue RegisterDataCollection(const T *collectionData, int count)
+    {
+        IndexValue newIndex = _vertexDataCollections.size();
+        vector<T> data(collectionData, collectionData + count);
+        _vertexDataCollections.push_back(data);
+        return newIndex;
     }
 
     IndexValue RegisterDataCollection(vector<T> vertexData)
@@ -48,6 +78,11 @@ public:
         IndexValue newIndex = _vertexDataCollections.size();
         _vertexDataCollections.push_back(vertexData);
         return newIndex;
+    }
+
+    void UseDataCollection(const vector<T> &vertexData)
+    {
+        bindUnindexedData(vertexData);
     }
 
     unsigned int currentSize() const
@@ -82,6 +117,13 @@ private:
             unconditionalBindData(index);
     }
 
+    void bindUnindexedData(const vector<T> &vertexData)
+    {
+        _bindData = &VertexArrayBuffer::initialBindData;
+        Bind();
+        glBufferData(_targetType, vertexData.size() * sizeof(T), vertexData.data(), GL_STATIC_DRAW);
+    }
+
     void unconditionalBindData(IndexValue index)
     {
         _currentIndex = index;
@@ -89,6 +131,59 @@ private:
         vector<T> *vertexData = &_vertexDataCollections[index];
         glBufferData(_targetType, vertexData->size() * sizeof(T), vertexData->data(), GL_STATIC_DRAW);
     }
+};
+
+class TileMap
+{
+public:
+    TileMap(RawImageInfo *tilemapImage, GLuint textureId, unsigned int tileWidth, unsigned int tileHeight)
+        : _tilemapImage(tilemapImage), _textureId(textureId), _tileWidth(tileWidth), _tileHeight(tileHeight)
+    {
+    }
+
+    GLuint GetTextureId() const
+    {
+        return _textureId;
+    }
+
+    vector<float> GetUVsForTile(IndexValue x, IndexValue y)
+    {
+        vector<float> UVs;
+
+        unsigned int top, bottom, left, right;
+        top = y * _tileHeight;
+        bottom = (y + 1) * _tileHeight - 1;
+        left = x * _tileWidth;
+        right = (x + 1) * _tileWidth - 1;
+
+        float UVtop, UVbottom, UVleft, UVright;
+        UVleft = (float) left / _tilemapImage->width;
+        UVright = (float) right / _tilemapImage->width;
+        UVtop = (float) top / _tilemapImage->height;
+        UVbottom = (float) bottom / _tilemapImage->height;
+
+        UVs.push_back(UVright);
+        UVs.push_back(UVtop);
+        UVs.push_back(UVleft);
+        UVs.push_back(UVtop);
+        UVs.push_back(UVleft);
+        UVs.push_back(UVbottom);
+
+        UVs.push_back(UVright);
+        UVs.push_back(UVtop); 
+        UVs.push_back(UVleft);
+        UVs.push_back(UVbottom);
+        UVs.push_back(UVright);
+        UVs.push_back(UVbottom);
+
+        return UVs;
+    }
+
+private:
+    RawImageInfo *_tilemapImage;
+    GLuint _textureId;
+    unsigned int _tileWidth;
+    unsigned int _tileHeight;
 };
 
 class ADSRendererImplementation : public ADSRenderer::IADSRendererImplementation
@@ -123,8 +218,6 @@ public:
         glEnableVertexAttribArray(1);
         glEnableVertexAttribArray(2);
 
-        _projectionMatrix = glm::perspective(45.0f, 4.0f/3.0f, 1.0f, 100.0f);
-
         _positionBuffer.SetUp(0, 4, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL);
         _normalBuffer.SetUp(1, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL);
         _uvBuffer.SetUp(2, 2, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL);
@@ -133,6 +226,10 @@ public:
         _normalMatrixLocation = glGetUniformLocation(_shaderProgramHandle, "NormalMatrix");
         _projectionMatrixLocation = glGetUniformLocation(_shaderProgramHandle, "ProjectionMatrix");
         _MVPMatrixLocation = glGetUniformLocation(_shaderProgramHandle, "MVP");
+
+        _imageVertexIndex = _positionBuffer.RegisterDataCollection(imageVertices, 24);
+        _imageNormalIndex = _normalBuffer.RegisterDataCollection(imageNormals, 18);
+        _imageIndexIndex = _indexBuffer.RegisterDataCollection(imageIndices, 6);
     }
     
     void Use()
@@ -159,6 +256,16 @@ public:
         return index;
     }
 
+    IndexValue RegisterTileMap(RawImageInfo *tilemapImage, unsigned int tileWidth, unsigned int tileHeight)
+    {
+        GLuint textureId = registerTexture(tilemapImage);
+        TileMap map(tilemapImage, textureId, tileWidth, tileHeight);
+
+        IndexValue tileMapId = _tileMaps.size();
+        _tileMaps.push_back(map);
+        return tileMapId;
+    }
+
     void SetModelMatrix(const glm::mat4 &matrix)
     {
         _modelMatrix = matrix;
@@ -168,12 +275,18 @@ public:
         _MVPMatrix = _projectionMatrix * _modelViewMatrix;
     }
 
-    void SetViewMatrix(glm::mat4 matrix)
+    void SetViewMatrix(const glm::mat4 &matrix)
     {
         _viewMatrix = matrix;
 
         _modelViewMatrix = _viewMatrix * _modelMatrix;
         _normalMatrix = glm::transpose(glm::inverse(glm::mat3(_modelViewMatrix)));
+        _MVPMatrix = _projectionMatrix * _modelViewMatrix;
+    }
+
+    void SetProjectionMatrix(const glm::mat4 &matrix)
+    {
+        _projectionMatrix = matrix;
         _MVPMatrix = _projectionMatrix * _modelViewMatrix;
     }
 
@@ -186,10 +299,37 @@ public:
 
         glm::vec4 viewLightPosition = _modelViewMatrix * _light.position;
 
-        setUniform("Light.Position", &viewLightPosition);
+        setUniform("Light.Position", viewLightPosition);
         setUniform("Light.La", glm::value_ptr(_light.La));
         setUniform("Light.Ld", glm::value_ptr(_light.Ld));
         setUniform("Light.Ls", glm::value_ptr(_light.Ls));
+    }
+
+    void RenderTile(IndexValue tilemapId, IndexValue x, IndexValue y)
+    {
+        glBindVertexArray(_vaoHandle);
+
+        glUniformMatrix4fv(_modelViewMatrixLocation, 1, GL_FALSE, glm::value_ptr(_modelViewMatrix));
+        glUniformMatrix3fv(_normalMatrixLocation, 1, GL_FALSE, glm::value_ptr(_normalMatrix));
+        glUniformMatrix4fv(_projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(_projectionMatrix));
+        glUniformMatrix4fv(_MVPMatrixLocation, 1, GL_FALSE, glm::value_ptr(_MVPMatrix));
+
+        _indexBuffer.UseDataCollection(_imageIndexIndex);
+        _positionBuffer.UseDataCollection(_imageVertexIndex);
+        _normalBuffer.UseDataCollection(_imageNormalIndex);
+        _uvBuffer.UseDataCollection(_tileMaps[tilemapId].GetUVsForTile(x, y));
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, _tileMaps[tilemapId].GetTextureId());
+
+        setUniform("Material.Ka", glm::vec3(0.0f, 0.0f, 0.0f));
+        setUniform("Material.Kd", glm::vec3(1.0f, 1.0f, 1.0f));
+        setUniform("Material.Ks", glm::vec3(0.0f, 0.0f, 0.0f));
+        setUniform("Material.Shininess", 1.0f);
+        setUniform("Material.Kd_map", 0);
+        setUniform("Material.hasKdMap", (int)true);
+
+        glDrawArrays(GL_TRIANGLES, 0, _indexBuffer.currentSize());
     }
 
     void Render(const Model *model)
@@ -226,6 +366,7 @@ private:
     VertexArrayBuffer<float> _uvBuffer;
 
     vector<MaterialInfo> _materials;
+    vector<TileMap> _tileMaps;
     
     GLuint _vertexShaderHandle;
     GLuint _fragmentShaderHandle;
@@ -244,6 +385,12 @@ private:
     glm::mat4 _MVPMatrix;
 
     glm::mat3 _normalMatrix;
+
+    IndexValue _imageVertexIndex;
+    IndexValue _imageNormalIndex;
+    IndexValue _imageUVIndex;
+    IndexValue _imageIndexIndex;
+    IndexValue _imageMaterialIndex;
     
 private:
     GLuint registerTexture(RawImageInfo *imageInfo)
@@ -285,16 +432,16 @@ private:
         glUniform3fv(uniformLocation, 1, info);
     }
 
-    void setUniform(const char *uniformName, const glm::vec3 *info)
+    void setUniform(const char *uniformName, const glm::vec3 &info)
     {
         GLuint uniformLocation = glGetUniformLocation(_shaderProgramHandle, uniformName);
-        glUniform3fv(uniformLocation, 1, glm::value_ptr(*info));
+        glUniform3fv(uniformLocation, 1, glm::value_ptr(info));
     }
 
-    void setUniform(const char *uniformName, const glm::vec4 *info)
+    void setUniform(const char *uniformName, const glm::vec4 &info)
     {
         GLuint uniformLocation = glGetUniformLocation(_shaderProgramHandle, uniformName);
-        glUniform4fv(uniformLocation, 1, glm::value_ptr(*info));
+        glUniform4fv(uniformLocation, 1, glm::value_ptr(info));
     }
 
     string GetShaderLog(GLuint shaderHandle)
@@ -401,19 +548,34 @@ Model *ADSRenderer::CreateModelFromImporter(IRenderer::Importer &importer)
     return _implementation->CreateModelFromImporter(importer);
 }
 
+IndexValue ADSRenderer::RegisterTileMap(RawImageInfo *tilemapImage, unsigned int tileWidth, unsigned int tileHeight)
+{
+    return _implementation->RegisterTileMap(tilemapImage, tileWidth, tileHeight);
+}
+
 void ADSRenderer::SetModelMatrix(const glm::mat4 &matrix)
 {
     _implementation->SetModelMatrix(matrix);
 }
 
-void ADSRenderer::SetViewMatrix(glm::mat4 matrix)
+void ADSRenderer::SetViewMatrix(const glm::mat4 &matrix)
 {
     _implementation->SetViewMatrix(matrix);    
+}
+
+void ADSRenderer::SetProjectionMatrix(const glm::mat4 &matrix)
+{
+    _implementation->SetProjectionMatrix(matrix);
 }
 
 void ADSRenderer::SetLight(LightInfo info)
 {
     _implementation->SetLight(info);
+}
+
+void ADSRenderer::RenderTile(IndexValue tilemapId, IndexValue x, IndexValue y)
+{
+    _implementation->RenderTile(tilemapId, x, y);
 }
 
 void ADSRenderer::Render(const Model *model)
--- src/adsrenderer.hh
+++ src/adsrenderer.hh
@@ -15,10 +15,13 @@ public:
     
     void Use();
     Model *CreateModelFromImporter(IRenderer::Importer &importer);
+    IndexValue RegisterTileMap(RawImageInfo *tilemapImage, unsigned int tileWidth, unsigned int tileHeight);
     void SetModelMatrix(const glm::mat4 &matrix);
-    void SetViewMatrix(glm::mat4 matrix);
+    void SetViewMatrix(const glm::mat4 &matrix);
+    void SetProjectionMatrix(const glm::mat4 &matrix);
     void SetLight(LightInfo info);
 
+    void RenderTile(IndexValue tilemapId, IndexValue x, IndexValue y);
     void Render(const Model *model);
 
 public:
@@ -28,9 +31,13 @@ public:
         virtual ~IADSRendererImplementation() {}
         virtual void Use() = 0;
         virtual Model *CreateModelFromImporter(IRenderer::Importer &importer) = 0;
+        virtual IndexValue RegisterTileMap(RawImageInfo *tilemapImage, unsigned int tileWidth, unsigned int tileHeight) = 0;
         virtual void SetModelMatrix(const glm::mat4 &matrix) = 0;
-        virtual void SetViewMatrix(glm::mat4 matrix) = 0;
+        virtual void SetViewMatrix(const glm::mat4 &matrix) = 0;
+        virtual void SetProjectionMatrix(const glm::mat4 &matrix) = 0;
         virtual void SetLight(LightInfo info) = 0;
+
+        virtual void RenderTile(IndexValue tilemapId, IndexValue x, IndexValue y) = 0;
         virtual void Render(const Model *model) = 0;
     };
 
